a6449b6e47a3b1759d4e67489d3b749ddf79a41f
harinadh76 <tannaharinadh@gmail.com>
2022-04-14 11:06:00 +0530

    files

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "images/dice" + randomNumber + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src" , randomDiceImage);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1 ];
+
+image2.setAttribute("src" , randomDiceImage2);
+
+
+if(randomNumber > randomNumber2)
+{
+  document.querySelector("h1").innerHTML= "Player1 wins 🚩";
+}
+else if(randomNumber2 > randomNumber) {
+  document.querySelector("h1").innerHTML = "Player2 wins 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw";
+
+}
